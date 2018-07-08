1302d2016fc2a0f0a37fbd2aa917d28bc7ea78f3
chenweigao <297859260@qq.com>
2018-07-08 13:26:07 +0800

    add difination

--- ticketmachine/TicketMachine.h
+++ ticketmachine/TicketMachine.h
@@ -4,5 +4,16 @@ class TicketMachine
 public:
 	TicketMachine();
 	virtual ~TicketMachine();
+	void showPrompt();
+	void insertMoney(int money);
+	void showBalence();
+	void printTicket();
+	void showTotal();
+	int refundBalance();
+	//declaration
+private:
+	const int PRICE;
+	int balance;
+	int total;
 };
 
